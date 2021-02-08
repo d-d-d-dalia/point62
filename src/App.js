2f0393d686348816e9cc5aba76698f57758cab73
Dalia <dalia.sawaya@flatironschool.com>
2021-02-08 13:29:53 -0500

    import HomePage component in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import './styles/App.css';
+import React, { Component } from 'react' //do we need this?
+import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <HomePage />
+    </main>
   );
 }
 
