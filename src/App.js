622598c6852a213620eac0cc52ec48a267596dd6
alicekb <alicekb01@gmail.com>
2021-02-12 12:43:51 -0500

    Setup initial html and grid layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <main className="container">
+    <main className="grid grid-cols-2 grid-rows-3
+    md:container md:mx-auto mx-w-2xl">
       <HomePage />
     </main>
   );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,12 @@
 const HomePage = () => {
   return <>
-    <header>
+    <header className="col-span-2 row-start-1 text-center">
       <h1>Welcome to point62! </h1>
       <p>Develop a sense of distance in kilometers for use in your every day life!</p>
     </header>
-    <section>
-      <h2>How to Play</h2>
+    <section className="col-span-2
+    justify-self-center">
+      <h2 className="text-center underline">How to Play</h2>
       <ul>
         <li>Enter 2 locations in the form below</li>
         <li>Check out the route on the map</li>
@@ -15,9 +16,19 @@ const HomePage = () => {
       <p>If it's too far off, congrats! You get to try again! (If you want a hint, we'll allow it.)</p>
     </section>
 
-    <img src={`https://maps.googleapis.com/maps/api/staticmap?size=500x500
-&markers=color:blue%7Clabel:A%7CDelta+Junction,AK
-&markers=color:blue%7Clabel:B%7CNew+York,NY&key=${process.env.REACT_APP_GOOGLE_API_KEY} `} alt="map displaying markers for Delta Junction, AK and New York, NY" />
+    <form className="flex flex-col">
+      <label htmlFor="point-a">Point A:</label>
+      <input id="point-a" type="text" name="point-a" className="border-2 border-black" />
+      <label htmlFor="point-b">Point B:</label>
+      <input id="point-b" type="text" name="point-a" className="border-2 border-black" />
+      <input type="submit" value="submit" />
+    </form>
+
+    { true ? <p>loading...</p> :
+      <img src={`https://maps.googleapis.com/maps/api/staticmap?size=500x500
+  &markers=color:blue%7Clabel:A%7CDelta+Junction,AK
+  &markers=color:blue%7Clabel:B%7CNew+York,NY&key=${process.env.REACT_APP_GOOGLE_API_KEY} `} alt="map displaying markers for Delta Junction, AK and New York, NY" />}
+
 
   </>
 }
