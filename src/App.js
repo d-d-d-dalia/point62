9734032fe691696c224696d66792ba1983e42e44
alicekb <alicekb01@gmail.com>
2021-02-22 10:16:51 -0500

    Implement main grid layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <main className="grid grid-cols-2 grid-rows-3
-    md:container md:mx-auto mx-w-2xl">
+    <main className="md:container md:mx-auto mx-w-2xl">
       <HomePage />
     </main>
   );
