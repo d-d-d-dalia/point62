aa5ee3a9776ccc5e93e701c9af8387191814183a
alicekb <alicekb01@gmail.com>
2021-02-25 13:32:45 -0500

    Add React-Router-Don and Routes
    Co-authored-by: d-d-d-dalia <dalia.sawaya@flatironschool.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import HomePage from './pages/HomePage'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <main className="md:container md:mx-auto mx-w-2xl">
-      <HomePage />
-    </main>
+    <Router>
+      <main className="md:container md:mx-auto mx-w-2xl">
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route exact path="/leaderboard">
+            <p>Hello!</p>
+          </Route>
+        </Switch>
+
+      </main>
+    </Router>
   );
 }
 
