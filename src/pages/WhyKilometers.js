e30b7f4afdc000f4f42a80c2e712b49227818db0
Dalia <dalia.sawaya@flatironschool.com>
2021-02-28 11:28:02 -0500

    minor edit in whykilometers

--- src/pages/WhyKilometers.js
+++ src/pages/WhyKilometers.js
@@ -2,7 +2,7 @@ const WhyKilometers = () => {
 
   return <div className="flex flex-col">
     <h1 className="text-4xl text-center underline mb-3">Why Kilometers</h1>
-    <p className="text-xl">To date, the imperial system of measurement (think, miles, pounds, etc), is only used in 3 countries on the planet: Liberia, Myanmar, the United States, and the United Kingdom (though only for distance). Point62 aims to make it easier for mile-oriented people to get around all over the world, with a little less confusion and disorientation.</p>
+    <p className="text-xl">To date, the imperial system of measurement (think, miles, pounds, etc), is only used in 4 countries on the planet: Liberia, Myanmar, the United States, and the United Kingdom (though only for distance). Point62 aims to make it easier for mile-oriented people to get around all over the world, with a little less confusion and disorientation.</p>
     <p> <a href="https://en.wikipedia.org/wiki/Kilometre" className="hover:underline hover:text-blue-500">Read about the kilometer </a></p>
     <p> <a href="https://en.wikipedia.org/wiki/Mile" className="hover:underline hover:text-blue-500">Read about the mile </a></p>
   </div>
